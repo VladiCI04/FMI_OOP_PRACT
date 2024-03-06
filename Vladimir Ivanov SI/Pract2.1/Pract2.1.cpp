dc079f5ce4e28dfb1782a96379838701cf587848
Vladimir Ivanov <140197292+VladiCI04@users.noreply.github.com>
2024-03-06 20:24:30 +0200

    Add Pract2.1
    Add Pract2.1

---
+++ Vladimir Ivanov SI/Pract2.1/Pract2.1.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <fstream>
+#pragma warning(disable:4996)
+using std::cin;
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::ofstream;
+
+
+size_t getFileSize(ifstream& in) {
+	size_t current = in.tellg();
+	in.seekg(0, std::ios::end);
+
+	size_t size = in.tellg();
+	in.seekg(current, std::ios::beg);
+
+	return size;
+}
+
+void writeFile(const char* fileName, const char* finalFile, const size_t finalFileSize) {
+	if (!fileName) {
+		return;
+	}
+
+	ofstream out(fileName);
+	if (!out.is_open()) {
+		return;
+	}
+
+	out.write(finalFile, finalFileSize);
+	out.close();
+}
+
+void concatFiles(const char* fileName1, const char* fileName2) {
+	if (!fileName1 || !fileName2) {
+		return;
+	}
+
+	ifstream in1(fileName1);	
+	if (!in1.is_open()) {
+		return;
+	}
+
+	ifstream in2(fileName2);
+	if (!in2.is_open()) {
+		in1.close();
+		return;
+	}
+
+	size_t fileSize1 = getFileSize(in1);
+	char* file1 = new char[fileSize1 + 1];
+	in1.read(file1, fileSize1);
+	file1[fileSize1] = '\0';
+	in1.close();
+
+	size_t fileSize2 = getFileSize(in2);
+	char* file2 = new char[fileSize2 + 1];
+	in2.read(file2, fileSize2);
+	file2[fileSize2] = '\0';
+	in2.close();
+
+	size_t finalFileSize = fileSize1 + fileSize2;
+	char* finalFile = new char[finalFileSize + 1];
+	strcpy(finalFile, file1);
+	finalFile = strcat(file1, file2);
+	finalFile[finalFileSize] = '\0';
+	cout << finalFile << endl;
+
+	char outFileName[] = "outFile.txt";
+	writeFile(outFileName, finalFile, finalFileSize);
+	
+	delete[] file1;
+	delete[] file2;
+	delete[] finalFile;
+}
+
+int main()
+{
+	char inFileName1[] = "file1.txt";
+	char inFileName2[] = "file2.txt";
+
+    concatFiles(inFileName1, inFileName2);
+}
